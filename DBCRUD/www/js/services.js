63a8a312ef695646729efb10f6afa85bf7684536
premkumar <premkumar@oranges-iMac.local>
2016-09-09 15:21:22 +0100

    updated

--- DBCRUD/www/js/services.js
+++ DBCRUD/www/js/services.js
@@ -1,7 +1,18 @@
 angular.module('starter')
 
-.service('DashboardServices',function($q){
+.service('DashboardServices',function($q, $http){
 console.log('services');
-var a = 1;
-return a;
+var dataArr = '';
+
+ $http.get('http://127.0.0.1:8888/demoServices/index.php').then(
+      function(result) {
+        console.log("secces"+JSON.stringify(result.data));
+        var obj = JSON.stringify(result.data); 
+        dataArr = JSON.parse(obj);          
+        console.log(dataArr.employees[0].firstName);
+      }, function(err) {
+         console.log("error"+JSON.stringify(err));
+      });
+ 
+return a = 1;
 })
