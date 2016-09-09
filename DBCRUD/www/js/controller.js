292b8dce9fe9cb4e078c2985c06db434edc37f53
premkumar <premkumar@oranges-iMac.local>
2016-09-09 17:06:30 +0100

    stackoverflow.com/questions/20369377/how-to-use-http-get-in-angularjs-correctly-in-specific-for-an-external-api-cal option

--- DBCRUD/www/js/controller.js
+++ DBCRUD/www/js/controller.js
@@ -1,5 +1,10 @@
 angular.module('starter')
 
 .controller('DashboardCtrl',function($scope, $state, DashboardServices){
-console.log("controller");
+	$scope.data = null;
+	DashboardServices.getData(function(dataResponse){
+		$scope.data = dataResponse;		
+		console.log(JSON.stringify($scope.data));
+	});
+
 })
--- DBCRUD/www/js/services.js
+++ DBCRUD/www/js/services.js
@@ -1,18 +1,14 @@
 angular.module('starter')
 
 .service('DashboardServices',function($q, $http){
-console.log('services');
-var dataArr = '';
-
- $http.get('http://127.0.0.1:8888/demoServices/index.php').then(
-      function(result) {
-        console.log("secces"+JSON.stringify(result.data));
-        var obj = JSON.stringify(result.data); 
-        dataArr = JSON.parse(obj);          
-        console.log(dataArr.employees[0].firstName);
-      }, function(err) {
-         console.log("error"+JSON.stringify(err));
-      });
- 
-return a = 1;
+	this.getData  = function(callbackfunc){
+		$http({
+			method: 'GET',
+			url: 'http://127.0.0.1:8888/demoServices/index.php'
+		}).success(function(data){
+			callbackfunc(data)
+		}).error(function(){
+			alert("error");
+		});
+	}
 })
