22bea3363a677be3186363505b87895baa4b9e5b
premkumar <premkumar@oranges-iMac.local>
2016-09-12 11:51:20 +0100

    the app is showing the data view using service layer which processing the json file

--- DBCRUD/www/js/controller.js
+++ DBCRUD/www/js/controller.js
@@ -2,9 +2,12 @@ angular.module('starter')
 
 .controller('DashboardCtrl',function($scope, $state, DashboardServices){
 	$scope.data = null;
+	$scope.username = "prem";
 	DashboardServices.getData().then(function(dataResponse){
-		$scope.data = dataResponse.data;		
-		console.log(JSON.stringify($scope.data));
+		$scope.items = dataResponse.data.employees;		
+		console.log(JSON.stringify($scope.items));		
+		$scope.username = $scope.items[0].firstName;
+	},function(err){
+		console.log("Error :"+JSON.stringify(err));
 	});
-
 })
